bdfe4ef05b42295edc12d81d5d4b46c8ca45ff53
Kienbeo <meoluoi95.1508@gmail.com>
2016-09-01 21:25:21 +0700

    First commit

---
+++ models/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url': 'mongodb://kienbeo:root@jello.modulusmongo.net:27017/hyr3eHid'
+}
---
+++ models/unit.js
@@ -0,0 +1,26 @@
+var mongoose = required('mongoose');
+
+var schema = mongoose.Shema({
+    name: String,
+    content: String,
+    week: Integer,
+    answer: String,
+    answer1: String,
+    answer2: String,
+    answer3: String,
+    answer4: String
+});
+
+function createunit(n, c, w, a, a1, a2, a3, a4 ) {
+    this.name= n;
+    this.content= c;
+    this.week= w;
+    this.answer= a;
+    this.answer1= a1;
+    this.answer2= a2;
+    this.answer3= a3;
+    this.answer4= a4;
+}
+
+module.exports = mongoose.model('Unit', schema);
+
